38a71a5037eab9b61fc32e18686c75d802e09b78
petter gulliksen <petter-gulliksen@hotmail.com>
2019-09-19 14:34:52 +0200

    created the routes and the controller

---
+++ app/controllers/dose_controller.rb
@@ -0,0 +1,7 @@
+class DoseController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'dose/new'
+  get 'dose/create'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:edit, :destroy]
 end
