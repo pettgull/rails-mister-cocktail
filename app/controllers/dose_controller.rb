160281ee1978fc7499d8e7764e9f9d41e6e68a3e
petter gulliksen <petter-gulliksen@hotmail.com>
2019-09-20 11:30:09 +0200

    doing some refactoring

--- app/controllers/dose_controller.rb
+++ app/controllers/dose_controller.rb
@@ -1,22 +1,33 @@
 class DoseController < ApplicationController
+   before_action :set_cocktail, only: [:new, :create]
+
   def new
     @dose = Dose.new
   end
 
   def create
-    # @cocktail = Cocktail.find(params[:cocktail_id])
-    # @dose = Dose.new(dose_params)
-    # @dose.cocktail = @cocktail
-    # if @dose.save
-    #   redirect_to cocktail_path(@cocktail)
-    # else
-    #   render "cocktails/show"
-    # end
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    dose = Dose.find(params[:id])
+    dose.destroy
+    redirect_to cocktails_path
   end
 
   private
 
   def dose_params
     params.require(:dose).permit(:description)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates_associated :cocktail, :ingredient, uniqueness: true
   validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
